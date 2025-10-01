ebea239e256140d6a7140bea144afd73afbfd0a0
Jesus Bejar <israelbejarii@gmail.com>
2025-10-01 12:21:47 -0600

    miscellaneous commit

--- src/hooks/useLocationTracking.ts
+++ src/hooks/useLocationTracking.ts
@@ -1,6 +1,6 @@
-import { useCallback, useEffect, useState } from 'react';
-import { Alert } from 'react-native';
-import locationService from '../services/locationService';
+import { useCallback, useEffect, useState } from "react";
+import { Alert } from "react-native";
+import locationService from "../services/locationService";
 
 export interface UseLocationTrackingReturn {
   isTracking: boolean;
