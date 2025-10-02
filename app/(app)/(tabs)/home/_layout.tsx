df59fc9fac6243e35f7d6ac823a96f3352d27a2e
Vicente <vicenteferrer_01@hotmail.com>
2025-10-01 22:04:29 -0300

    Testing location

--- app/(app)/(tabs)/home/_layout.tsx
+++ app/(app)/(tabs)/home/_layout.tsx
@@ -1,52 +1,56 @@
+import { LocationProvider } from '@/src/core/context/LocationContext';
 import { Stack } from 'expo-router';
 
 export default function HomeLayout() {
-  return (
-    <Stack>
-      <Stack.Screen 
-        name="index" 
-        options={{ 
-          headerShown: false,
-          title: 'Home'
-        }} 
-      />
-      <Stack.Screen 
-        name="booking" 
-        options={{ 
-          title: 'Book a Ride',
-          headerBackVisible: false,
-          headerLargeTitle: false,
-          //headerBackTitleVisible: false,
-        }} 
-      />
-      <Stack.Screen 
-        name="ride-tracking" 
-        options={{ 
-          title: 'Your Ride',
-          headerBackVisible: false,
-          headerLargeTitle: false,
-          //headerBackTitleVisible: false,
-          gestureEnabled: false,
-        }} 
-      />
-      <Stack.Screen 
-        name="destination" 
-        options={{ 
-          title: 'Where to?',
-          headerBackVisible: false,
-          headerLargeTitle: false,
-          //headerBackTitleVisible: false,
-        }} 
-      />
-      <Stack.Screen 
-        name="payment" 
-        options={{ 
-          title: 'Payment',
-          headerBackVisible: false,
-          headerLargeTitle: false,
-          //headerBackTitleVisible: false,
-        }} 
-      />
-    </Stack>
-  );
+	return (
+		<LocationProvider>
+			<Stack>
+				<Stack.Screen
+					name='index'
+					options={{
+						headerShown: false,
+						title: 'Home'
+					}}
+				/>
+
+				<Stack.Screen
+					name='booking'
+					options={{
+						title: 'Book a Ride',
+						headerBackVisible: false,
+						headerLargeTitle: false
+						//headerBackTitleVisible: false,
+					}}
+				/>
+				<Stack.Screen
+					name='ride-tracking'
+					options={{
+						title: 'Your Ride',
+						headerBackVisible: false,
+						headerLargeTitle: false,
+						//headerBackTitleVisible: false,
+						gestureEnabled: false
+					}}
+				/>
+				<Stack.Screen
+					name='destination'
+					options={{
+						title: 'Where to?',
+						headerBackVisible: false,
+						headerLargeTitle: false
+						//headerBackTitleVisible: false,
+					}}
+				/>
+				<Stack.Screen
+					name='payment'
+					options={{
+						title: 'Payment',
+						headerBackVisible: false,
+						headerLargeTitle: false
+						//headerBackTitleVisible: false,
+					}}
+				/>
+			</Stack>
+		</LocationProvider>
+	);
 }
