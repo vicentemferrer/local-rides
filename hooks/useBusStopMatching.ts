cf91c5dc39da9c6d18e2d145adcdad84f591546b
Jesus Bejar <israelbejarii@gmail.com>
2025-10-03 16:35:06 -0600

    added custom hook logic

--- hooks/useBusStopMatching.ts
+++ hooks/useBusStopMatching.ts
@@ -1,1 +1,24 @@
+import { BusStopWithDistance, findClosestBusStop } from '@/src/core/api/busStopService';
+import { PlaceDetails } from '@/src/core/api/placesService';
+import { useState } from 'react';
 
+export function useBusStopMatching() {
+    const [closestBusStop, setClosestBusStop] = useState<BusStopWithDistance | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    const findClosestStopToDestination = async (destination: PlaceDetails) => {
+      setIsLoading(true);
+      try {
+        const closestStop = await findClosestBusStop(
+          destination.geometry.location.lat,
+          destination.geometry.location.lng
+        );
+        setClosestBusStop(closestStop);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    return { closestBusStop, isLoading, findClosestStopToDestination };
+  }
+  
