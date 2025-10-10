415821c15c93d5bdb272aea1c57919a440616fd2
Jesus Bejar <israelbejarii@gmail.com>
2025-10-10 14:18:01 -0600

    added fake dependencies & fake fetch

---
+++ __tests__/placesService.test.ts
@@ -0,0 +1,12 @@
+
+// fake dependencies
+jest.mock('expo-constants', () => ({
+  expoConfig: {
+    extra: {
+      googlePlacesApiKey: 'test-api-key'
+    }
+  }
+}));
+
+// fake fetch
+global.fetch = jest.fn();
