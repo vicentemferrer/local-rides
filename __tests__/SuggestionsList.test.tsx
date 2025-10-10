33c54c5bc3002e67ca70cee421d0d6cf8ecb3c79
Jesus Bejar <israelbejarii@gmail.com>
2025-10-10 14:10:08 -0600

    added third test case

--- __tests__/SuggestionsList.test.tsx
+++ __tests__/SuggestionsList.test.tsx
@@ -31,4 +31,18 @@ describe('SuggestionsList', () => {
     expect(queryByText('New York')).toBeFalsy();
   });
 
+  it('handles multiple selections', () => {
+    const mockOnSelect = jest.fn();
+    const { getByText } = render(
+      <SuggestionsList predictions={mockPlacePredictions} onSelect={mockOnSelect} />
+    );
+    
+    fireEvent.press(getByText('New York'));
+    fireEvent.press(getByText('Newark'));
+    
+    expect(mockOnSelect).toHaveBeenCalledTimes(2);
+    expect(mockOnSelect).toHaveBeenNthCalledWith(1, mockPlacePredictions[0]);
+    expect(mockOnSelect).toHaveBeenNthCalledWith(2, mockPlacePredictions[1]);
+  });
+
  
